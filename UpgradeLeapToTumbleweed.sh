66f80eb826555e57b247c98620ded5dc29aeca95
Aleš Pestotnik <167247195+alespestotnik@users.noreply.github.com>
2025-02-25 22:50:08 +0100

    Update UpgradeLeapToTumbleweed.sh

--- UpgradeLeapToTumbleweed.sh
+++ UpgradeLeapToTumbleweed.sh
@@ -57,9 +57,7 @@ fi
 # Step 2: Remove existing Leap repositories
 if [[ $(cat "$FLAG_FILE") -eq 1 ]]; then
     log "Removing old openSUSE Leap repositories..."
-	for repo in $(zypper lr -u | awk 'NR>2 {print $1}'); do
-    	zypper rr "$repo"
-	done
+    	zypper lr | awk 'NR>2 && $1+0==$1 {print $1}' | xargs -r zypper rr
     echo "2" > "$FLAG_FILE"
 fi
 
